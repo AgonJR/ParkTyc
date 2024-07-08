060142b1b03bee22c43485fd5c136f39f9f3e661
Agon <agonthedev@gmail.com>
2024-07-07 22:11:12 -0700

    Highlight Tile On Mouse Over
    - Created and added a simple script to the grid tile prefab to change the material when the mouse cursor hover over it
    
    - Added a plain colour material to use as the grid tile highlight

---
+++ Assets/Code/GridTile.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GridTile : MonoBehaviour
+{
+
+    public Material hoverMaterial;
+
+    private Material _originalMat; 
+    private Renderer _rendererRef;  
+
+    void Start()
+    {
+        _rendererRef = gameObject.GetComponent<MeshRenderer>();
+        _originalMat = _rendererRef.material;
+    }
+
+    void OnMouseEnter()
+    {
+        Debug.Log("MOUSE OVER " + gameObject.name);
+        if (hoverMaterial != null)
+        {
+            _rendererRef.material = hoverMaterial;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        _rendererRef.material = _originalMat; 
+    }
+}
