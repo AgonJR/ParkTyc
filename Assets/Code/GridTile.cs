baa897fcbb5f35e74720e0a7da1aca4b64c77742
Agon <agonthedev@gmail.com>
2024-07-08 16:50:39 -0700

    Use Highlight Tile Instead of Material Change
    - Added a new tile prefab with a transparent material to use as a highlight instead of changing the tile's material
    
    - Tile Script will instantiate ONE copy of the highlight tile as a static singleton to be shared and repositioned on mouse hover
    
    - Updated SceneZero to use the new BaseTile prefab instead of the placeholder HexTile

--- Assets/Code/GridTile.cs
+++ Assets/Code/GridTile.cs
@@ -2,28 +2,36 @@ using UnityEngine;
 
 public class GridTile : MonoBehaviour
 {
+    public GameObject highlightTile;
 
-    public Material hoverMaterial;
+    private Renderer _rendererRef;
+    private Vector3 _highlightPos;
 
-    private Material _originalMat; 
-    private Renderer _rendererRef;  
+    private static GameObject _highlightTileObject;
+
+    void Awake()
+    {
+        if (GridTile._highlightTileObject == null)
+        {
+            _highlightTileObject = Instantiate(highlightTile, Vector3.zero, highlightTile.transform.rotation);
+            _highlightTileObject.SetActive(false);
+        }
+    }
 
     void Start()
     {
         _rendererRef = gameObject.GetComponent<MeshRenderer>();
-        _originalMat = _rendererRef.material;
+        _highlightPos = new Vector3(gameObject.transform.position.x, 0.3f, gameObject.transform.position.z);
     }
 
     void OnMouseEnter()
     {
-        if (hoverMaterial != null)
-        {
-            _rendererRef.material = hoverMaterial;
-        }
+        _highlightTileObject.SetActive(true);
+        _highlightTileObject.transform.position = _highlightPos;
     }
 
     void OnMouseExit()
     {
-        _rendererRef.material = _originalMat; 
+        _highlightTileObject.SetActive(false);
     }
 }
