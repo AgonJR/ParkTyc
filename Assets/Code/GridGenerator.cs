fb391e71e8521c65dd3db4809fb18ebf1f9f1707
Agon <agonthedev@gmail.com>
2024-07-08 15:52:42 -0700

    GridGenerator Script Created
    - Instantiates and stores a 2D grid of game objects for the grid tiles
    - Added a boolean toggle to re-generate using a new grid size
    - A function calculates the position of each tile based on hard-coded values derived from manually placing the placeholder tiles into place

---
+++ Assets/Code/GridGenerator.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    [Header("Data")]
+    public GameObject tilePrefab;
+
+    [Header("Dev Tools")]
+    public int gridSize = 3; //Assuming Square Grid
+    public bool regenerateGrid = true;
+
+    private GameObject[,] _grid;
+
+
+    private void Update()
+    {
+        if (regenerateGrid)
+        {
+            ClearGrid();
+            GenerateGrid();
+        }
+    }
+
+    private void ClearGrid()
+    {
+        regenerateGrid = false;
+
+        if ( _grid != null ) { foreach (GameObject tile in _grid) { Destroy(tile); } }
+
+        _grid = new GameObject[gridSize, gridSize];
+    }
+
+    private void GenerateGrid()
+    {
+
+        for (int q = 0; q < gridSize; q++) //column
+        {
+            for (int r = 0; r < gridSize; r++) //row
+            {
+                Vector3 position = CalculateTilePosition(q, r);
+
+                GameObject tile = Instantiate(tilePrefab, position, tilePrefab.transform.rotation);
+                tile.transform.parent = this.transform;
+
+                tile.name = "HexTile (" + q + " ," + r + ")";
+
+                _grid[q,r] = tile;
+            }
+        }
+    }
+
+    private Vector3 CalculateTilePosition(int q, int r)
+    {
+        //Hardcoded values match placeholder grid tile
+
+        float h = -8.70f;
+        float w = 7.535f;
+
+        float oddOffset = -4.35f;
+
+        float x = q * w; 
+        float z = r * h;
+
+        z += q % 2 == 0.0f ? 0.0f : oddOffset;
+
+        return new Vector3(x, 0.0f, z);
+    }
+}
