b1818d728469f0174f6739c35b81f1960b5f4dd1
Alex Cristoffanini <acristoffanini@gmail.com>
2024-07-31 22:36:50 -0700

    tooltip

--- Assets/Code/ButtonBehavior.cs
+++ Assets/Code/ButtonBehavior.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class ButtonBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject ToolTip;
+
+    private bool isActive = false;
+    public float timeToTip = 3.0f;
+    private float currentTimer;
+
+    public void onHover() {
+        // ToolTip.SetActive(true);
+        isActive = true;
+    }
+
+    public void exitHover() {
+        isActive = false;
+        ToolTip.SetActive(false);
+    }
+
+    private void setToolTip() {
+        ToolTip.SetActive(true);
+    }
+
     void Start()
     {
-        
+        ToolTip.SetActive(false);
+        currentTimer = timeToTip;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isActive) {
+            // Debug.Log("Start Timer!");
+            currentTimer -= Time.deltaTime;
+        }
+
+        if (currentTimer <= 0.0f) {
+            setToolTip();
+            currentTimer = timeToTip;
+            isActive = false;
+        }
     }
 }
