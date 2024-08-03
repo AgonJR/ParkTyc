4818e08eeaafcd69e6e6b5341e5bd308a293d7f9
Alex Cristoffanini <acristoffanini@gmail.com>
2024-08-02 23:14:39 -0700

    hopefully resolution change

--- Assets/Code/BottomPanelBehavior.cs
+++ Assets/Code/BottomPanelBehavior.cs
@@ -5,9 +5,12 @@ public class BottomPanelBehavior : MonoBehaviour
 {
     public GameObject BottomPanel;
     public TMP_Text Karat;
+    public GameObject KaratBox;
     public float speed = 200.0f;
     private Vector2 target;
     private Vector2 position;
+    private float timeout = 2.0f;
+    private bool active = false;
 
     void Start()
     {
@@ -19,16 +22,28 @@ public class BottomPanelBehavior : MonoBehaviour
     {
         float step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, target, step);
+        if (active) {
+            timeout -= Time.deltaTime;
+        }
+        if (timeout <= 0.0f) {
+            active = false;
+            onEnd();
+        }
     }
 
     public void onHover() 
     {
         target = new Vector2((Screen.width / 2), 50);
         Karat.text = "";
+        timeout = 2.0f;
+        active = true;
     }
     
     public void onEnd()
     {
+        if (active == true) {
+            return;
+        }
         Karat.text = "^";
         target = new Vector2((Screen.width / 2), -200);
     }
